83d11db1b7394a8cc3621be7cc3013bcb61f6221
Dariusz Malinowski <dariusz.m.malinowski@gmail.com>
2025-10-24 13:28:12 +0200

    added computer move

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
+
+
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+if (randomNumber == 1){
+    computerMove = 'KAMIEŃ';
+} else if (randomNumber == 2){
+    computerMove = 'PAPIER';
+} else if (randomNumber == 3){
+    computerMove = 'NORZYCE';
+} else {
+    computerMove = "nieznany ruch"
+}
+
+printMessage('jestem komputerem i zagrałem ' + computerMove);
+
+
 let playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to norzyce, to wygrywasz!');
